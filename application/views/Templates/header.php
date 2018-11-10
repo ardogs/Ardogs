e2ba7e67641de5f6dab50900d39aa736e7c207a7
ardogs <jesus.martinsamano@gmail.com>
2018-11-10 15:59:02 -0600

    Favicon

--- application/views/Templates/header.php
+++ application/views/Templates/header.php
@@ -14,7 +14,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
-    <link rel="icon" href="http://drive.google.com/uc?export=view&id=1w3wrgI_gw8bbpQPYunMjIfcCU_Ywht1C">
+    <link rel="icon" href="https://image.ibb.co/dEEbaA/baidu-logotipo-de-la-pata-2.png">
 
     <!--Script Dropdown--->
     <script>
