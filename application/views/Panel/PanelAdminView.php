ee5c417681ff3e14e6795f210d7980d1970de725
ardogs <jesus.martinsamano.@gmail.com>
2019-04-23 19:01:57 -0500

    Las nuevas vistas de correo estan en la carpeta de registro

---
+++ application/views/Panel/PanelAdminView.php
@@ -0,0 +1,110 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.parallax');
+        var instances = M.Parallax.init(elems);
+    });
+</script>
+
+
+<div class="parallax-container">
+    <div class="parallax slider">
+        <img src="https://i.imgur.com/Se6dcQl.jpg">
+        <ul class="slides">
+    </div>
+</div>
+
+<center>
+    <img width="200" height="200" class="responsive-img" src="https://image.ibb.co/nvtOvU/Logo_2.png" ALIGN="center">
+</center>
+
+
+
+
+<h3 ALIGN="center">Panel de Control</h3>
+<br>
+
+<h6 ALIGN="center">¡Bienvenido administrador!</h6>
+
+<p ALIGN="center">
+    Aquí podrás consultar todas las solicitudes que han hecho los centros de adopcion <br>
+    ¡Elige bien!
+
+</p>
+<br>
+
+<div class="row">
+    <div class="col offset-m2 m8  s12">
+
+
+        <table class="responsive-table centered striped">
+            <thead>
+                <tr>
+                    <th>Beneficiencia</th>
+                    <th>Dirección</th>
+                    <th>Responsable</th>
+                    <th>Comprobante</th>
+                    <th>Acción</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Angeles guardianes</td>
+                    <td>Av las Torres #9, Col Guadalape</td>
+                    <td>Miguel Torres Gonzalez</td>
+                    <td><a href="">Comprobante.pdf</a></td>
+                    <td>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Aprobar" data-position="top"><i class="material-icons Large" style="color: #43a047">check</i></a>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Rechazar" data-position="bottom"><i class="material-icons" style="color: #d32f2f">close</i></a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Peludos en casa</td>
+                    <td>Calle 74 pte #9 col. Naciones Unidas</td>
+                    <td>Reinaldo Zaragoza Contreras</td>
+                    <td><a href="">Archivo.pdf</a></td>
+                    <td>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Aprobar" data-position="top"><i class="material-icons Large" style="color: #43a047">check</i></a>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Rechazar" data-position="bottom"><i class="material-icons" style="color: #d32f2f">close</i></a>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>Pequeños traviesos</td>
+                    <td>Gran avenida #5 Con Nueva Guinea</td>
+                    <td>Amador Rodriguez Fernandez</td>
+                    <td><a href="">Comprobate_ar.pdf</a></td>
+                    <td>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Aprobar" data-position="top"><i class="material-icons Large" style="color: #43a047">check</i></a>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Rechazar" data-position="bottom"><i class="material-icons" style="color: #d32f2f">close</i></a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Arca de Noe</td>
+                    <td>San Baltazar #54 Col El pedregal </td>
+                    <td>Lourdes Benitez Contreras</td>
+                    <td><a href="">Verificacion.pdf</a></td>
+                    <td>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Aprobar" data-position="top"><i class="material-icons Large" style="color: #43a047">check</i></a>
+                        <a class="waves-effect Default tooltipped" data-tooltip="Rechazar" data-position="bottom"><i class="material-icons" style="color: #d32f2f">close</i></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+
+
+
+
+    </div>
+
+</div>
---
+++ application/views/Registro/CorreoRechazoView.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Confirmación de correo</title>
+
+
+    <style type="text/css">
+        @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+        /* Take care of image borders and formatting */
+
+        img {
+            max-width: 600px;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+        }
+
+        a {
+            text-decoration: none;
+            border: 0;
+            outline: none;
+        }
+
+        a img {
+            border: none;
+        }
+
+        /* General styling */
+
+        td,
+        h1,
+        h2,
+        h3 {
+            font-family: Helvetica, Arial, sans-serif;
+            font-weight: 400;
+        }
+
+        body {
+            -webkit-font-smoothing: antialiased;
+            -webkit-text-size-adjust: none;
+            width: 100%;
+            height: 100%;
+            color: #37302d;
+            background: #ffffff;
+        }
+
+        table {
+            background:
+        }
+
+        h1,
+        h2,
+        h3 {
+            padding: 0;
+            margin: 0;
+            color: #ffffff;
+            font-weight: 400;
+        }
+
+        h3 {
+            color: #212121;
+            font-size: 24px;
+        }
+    </style>
+
+    <style type="text/css" media="screen">
+        @media screen {
+
+            /* Thanks Outlook 2013! http://goo.gl/XLxpyl*/
+            td,
+            h1,
+            h2,
+            h3 {
+                font-family: 'Lato', 'Helvetica Neue', 'Arial', 'sans-serif' !important;
+            }
+        }
+    </style>
+
+    <style type="text/css" media="only screen and (max-width: 480px)">
+        /* Mobile styles */
+        @media only screen and (max-width: 480px) {
+            table[class="w320"] {
+                width: 320px !important;
+            }
+
+            table[class="w300"] {
+                width: 300px !important;
+            }
+
+            table[class="w290"] {
+                width: 290px !important;
+            }
+
+            td[class="w320"] {
+                width: 320px !important;
+            }
+
+            table[class*="w260"] {
+                width: 260px !important;
+            }
+
+            td[class*="mobile-center"] {
+                text-align: center !important;
+            }
+
+            td[class*="mobile-padding"] {
+                padding-left: 20px !important;
+                padding-right: 20px !important;
+                padding-bottom: 20px !important;
+            }
+
+            td[class*="mobile-block"] {
+                display: block !important;
+                width: 280px !important;
+                text-align: justify !important;
+                padding-bottom: 20px !important;
+            }
+
+            td[class*="mobile-img"] {
+                display: block !important;
+                width: 100% !important;
+                text-align: center !important;
+                padding-bottom: 20px !important;
+                width: 320px !important;
+            }
+        }
+    </style>
+
+
+
+
+
+
+</head>
+
+<body class="body" style="padding:0; margin:0; display:block; background:#ffffff; -webkit-text-size-adjust:none" bgcolor="#ffffff">
+    <table align="center" cellpadding="0" cellspacing="0" width="100%" height="100%">
+        <tr>
+            <td align="center" valign="top" bgcolor="#ffffff" width="100%">
+
+                <table cellspacing="0" cellpadding="0" width="100%">
+                    <tr>
+                        <td style="border-bottom: 3px " width="100%">
+                            <center>
+                                <table cellspacing="0" cellpadding="0" width="500" class="w320">
+                                    <tr>
+                                        <td valign="top" style="padding:10px 0; text-align:center;" class="mobile-center">
+                                            <img width="250" height="62" src="https://image.ibb.co/dtKSaK/logo_mail.png">
+                                        </td>
+                                    </tr>
+                                </table>
+                            </center>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td background="https://i.imgur.com/8ywe8RV.jpg" bgcolor="#0d0102" valign="top" style="background: url(https://i.imgur.com/8ywe8RV.jpg) no-repeat center; background-color: #0d0102; background-position: center;">
+                            <!--[if gte mso 9]>
+          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;height:303px;">
+            <v:fill type="tile" src="https://www.filepicker.io/api/file/mEwMJfoIQlKlv1u3CSfU" color="#0d0102" />
+            <v:textbox inset="0,0,0,0">
+          <![endif]-->
+                            <div>
+                                <center>
+                                    <table cellspacing="0" cellpadding="0" width="530" height="303" class="w320">
+                                        <tr>
+                                        </tr>
+                                    </table>
+                                </center>
+                            </div>
+                            <!--[if gte mso 9]>
+            </v:textbox>
+          </v:rect>
+          <![endif]-->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td valign="top">
+                            <center>
+                                <table cellspacing="0" cellpadding="30" width="500" class="w320">
+                                    <tr>
+                                        <td valign="top" style="border-bottom:1px solid #a1a1a1;">
+
+                                            <table cellspacing="0" cellpadding="0" width="100%">
+                                                <tr>
+                                                    <td style="text-align: center;">
+                                                        <h3>¡De verdad lo sentimos!</h3>
+                                                        <br>
+                                                    </td>
+                                                </tr>
+                                            </table>
+
+
+                                        </td>
+                                    </tr>
+                                </table>
+                                <table cellspacing="0" cellpadding="0" width="500" class="w320">
+                                    <tr>
+                                        <td>
+
+                                            <table cellspacing="0" cellpadding="0" width="500" class="w320">
+                                                <tr>
+                                                    <td style="padding-bottom:30px; padding-top:30px;" class="mobile-img">
+                                                        <table cellspacing="0" cellpadding="0" width="100%">
+                                                            <tr>
+                                                                <td style="padding-bottom:0px;" class="mobile-img">
+                                                                    <table cellspacing="0" cellpadding="0" width="120">
+                                                                        <tr>
+                                                                            <td style="vertical-align:middle; text-align:center;" class="mobile-img">
+                                                                                <img style="border:0px solid #a1a1a1;" src="https://image.ibb.co/hjpGkK/huella.png" width="90" height="90" alt="profile" />
+                                                                            </td>
+                                                                        </tr>
+
+                                                                    </table>
+                                                                </td>
+                                                                <td class="mobile-block mobile-padding" style="text-align:justify;">
+                                                                    En verdad lo sentimos, pero la información que nos proporcionaste no fue suficiente para ser parte de Ardogs <br><br>
+                                                                    Cuando quieras volver a intentar te estaremos esperando con los brazos abiertos. <br>
+                                                                    Agradecemos tu solicitud y valoramos tu tiempo.
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="mobile-padding">
+                                            <table cellspacing="0" cellpadding="0" width="100%">
+                                                <tr>
+
+                                                    <td>
+                                                        &nbsp;
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <table cellspacing="0" cellpadding="25" width="100%">
+                                                <tr>
+                                                    <td>
+                                                        &nbsp;
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </center>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="background-color:#212121;">
+                            <center>
+                                <table cellspacing="0" cellpadding="0" width="500" class="w320">
+                                    <tr>
+                                        <td>
+                                            <table cellspacing="0" cellpadding="30" width="100%">
+                                                <tr>
+                                                    <td style="text-align:center;">
+                                                        <a href="#">
+                                                            <img width="41" height="41" src="https://image.ibb.co/n2OQUe/facebook_mail.png" alt="Facebook" />
+                                                        </a>
+                                                        <a href="#">
+                                                            <img width="41" height="41" src="https://image.ibb.co/g1MOFK/twitter_mail.png" alt="Twitter" />
+
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <center>
+                                                <table style="margin:0 auto;" cellspacing="0" cellpadding="5" width="100%">
+                                                    <tr>
+                                                        <td style="text-align:center; margin:0 auto;" width="100%">
+                                                            <a href="https://ardogs.herokuapp.com" style="text-align:center;">
+                                                                <img style="margin:0 auto;" width="123" height="24" src="https://image.ibb.co/fxcK5K/pie_mail.png" alt="logo link" />
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </center>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </center>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+    <script type="text/javascript">
+        document.oncontextmenu = function() {
+            return false;
+        }
+    </script>
+</body>
+
+</html>
